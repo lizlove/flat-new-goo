2f21f41d8f8d537ac25ef5194e83383de5b0a56f
lizlove <elizabethlovero@gmail.com>
2021-12-31 16:28:56 -0500

    Webpack sass fix.

--- webpack.dev.js
+++ webpack.dev.js
@@ -9,25 +9,25 @@ module.exports = merge(common, {
       overlay: true,
     },
   },
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.(sa|sc|c)ss$/i,
-  //       use: [
-  //         "style-loader",
-  //         {
-  //           loader: "css-loader",
-  //           options: {
-  //             importLoaders: 2,
-  //             // 0 => no loaders (default);
-  //             // 1 => postcss-loader;
-  //             // 2 => postcss-loader, sass-loader
-  //           },
-  //         },
-  //         "postcss-loader",
-  //         "sass-loader",
-  //       ],
-  //     },
-  //   ],
-  // },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 2,
+              // 0 => no loaders (default);
+              // 1 => postcss-loader;
+              // 2 => postcss-loader, sass-loader
+            },
+          },
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
 });
